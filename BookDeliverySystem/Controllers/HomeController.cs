2c1603f0fd801e4378a397908e6e819db8729cb9
Xallumi <Xallumi@DESKTOP-GG5B8TO>
2024-02-28 23:53:31 +0200

    fixed index page

--- BookDeliverySystem/Controllers/HomeController.cs
+++ BookDeliverySystem/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using BookDeliverySystem.Models;
+﻿using BookDeliverySystem.Areas.Identity.Data;
+using BookDeliverySystem.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,37 @@ namespace BookDeliverySystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SignInManager<ApplicationUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager; 
         }
 
-        public IActionResult Index()
+        public async Task<string> getUserRole()
         {
-            return View();
+            string? userId = HttpContext.User.Identity.Name;
+            if (userId == null || userId.Length == 0)
+            {
+                return null;
+            }
+            ApplicationUser user = await _signInManager.UserManager.FindByNameAsync(userId);
+            return user.Role;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string? role =await getUserRole();
+            if(role == "CLIE")
+            {
+                return View();
+            }
+            else
+            {
+                //Create error page
+                return View();
+            }
         }
 
 
