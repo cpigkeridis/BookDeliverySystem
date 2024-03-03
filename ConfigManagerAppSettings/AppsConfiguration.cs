e6f676a899bb137e1e6b355d913273065208b76f
Xallumi <Xallumi@DESKTOP-GG5B8TO>
2024-03-03 18:58:47 +0200

    Config Manager Settings

---
+++ ConfigManagerAppSettings/AppsConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigManagerAppSettings
+{
+    public class AppsConfiguration : IConfigManager
+    {
+        private readonly IConfiguration _configuration;
+        public AppsConfiguration(IConfiguration configuration) { 
+        this._configuration = configuration;
+        }
+
+        public string APICONSTRING
+        {
+            get
+            {
+                return this._configuration["ConnectionStrings:BookDeliverySystem"];
+            }
+        }
+    }
+}
