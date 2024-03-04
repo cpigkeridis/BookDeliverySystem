e92d1bd54f370fef2491293a846862fa29ef331e
Xallumi <Xallumi@DESKTOP-GG5B8TO>
2024-03-04 18:11:45 +0200

    fixed layout, authorization

--- BookDeliverySystem/Controllers/MenuController.cs
+++ BookDeliverySystem/Controllers/MenuController.cs
@@ -1,9 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookDeliverySystem.Areas.Identity.Data;
+using BookDeliverySystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BookDeliverySystem.Controllers
 {
     public class MenuController : Controller
     {
+
+        private readonly ILogger<MenuController> _logger;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public MenuController(ILogger<MenuController> logger, SignInManager<ApplicationUser> signInManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+        }
+
+        public async Task<string> getUserRole()
+        {
+            string? userId = HttpContext.User.Identity.Name;
+            if (userId == null || userId.Length == 0)
+            {
+                return null;
+            }
+            ApplicationUser user = await _signInManager.UserManager.FindByNameAsync(userId);
+            return user.Role;
+        }
+
+        public async Task<string> setUserRole(string role)
+        {
+            string? userId = HttpContext.User.Identity.Name;
+            ApplicationUser user = await _signInManager.UserManager.FindByNameAsync(userId);
+            user.Role = role;
+            // Save changes to the database
+            var result = await _signInManager.UserManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return role;
+            }
+            else
+            {
+                // Handle error if update fails
+                return "Failed to update user role";
+            }
+        }
+
         public IActionResult ContactUs()
         {
             return View();
@@ -12,13 +55,40 @@ namespace BookDeliverySystem.Controllers
         {
             return View();
         }
-        public IActionResult AdminModule()
+        public async Task<IActionResult> AdminModule()
         {
-            return View();
+            if (_signInManager.IsSignedIn(User))
+            {
+                //TODO if (!User.Identity.IsAuthenticated) custom function to authenticate based on enabled column
+                if (await getUserRole() != "ADMI")
+                {
+                    return RedirectToAction("AccessDenied", "Error");
+                }
+                //await setUserRole("ADMI");
+                return View();
+
+            }
+            else
+            {
+                return RedirectToAction("AccessDenied", "Error");
+            }
+
         }
         public IActionResult MyOrders()
         {
-            return View();
+            if (_signInManager.IsSignedIn(User))
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("AccessDenied", "Error");
+            }           
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
