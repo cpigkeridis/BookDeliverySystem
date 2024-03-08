4526e36fe1e9c98c7f02e5602d5b685e815ea118
Xallumi <Xallumi@DESKTOP-GG5B8TO>
2024-03-08 19:33:48 +0200

    edit form for clients

--- BookDeliverySystem/Models/SearchUsersReqModel.cs
+++ BookDeliverySystem/Models/SearchUsersReqModel.cs
@@ -3,5 +3,13 @@
     public class SearchUsersReqModel
     {
         public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string Role { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Enabled { get; set; }
+        public string OldRole { get; set; }
     }
 }
